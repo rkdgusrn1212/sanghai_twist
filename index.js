23d28ab0851366e209b992f9019a19ca12773894
integer <integer402@gmail.com>
2022-12-07 21:19:30 +0900

    merge

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('상하의 트위스트 서버 메인');
-});
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`sanghai-twist server is listening on port ${port}`);
