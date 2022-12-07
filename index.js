57eabe1c50724116791d95c9fb4cc8f47f9ec3ac
rkdgusrn1212 <khgkjg12@naver.com>
2022-12-07 16:07:05 +0900

    리엑트 프론트앱 경로를 NodeJs에서 라우팅하게 설정
    - React 전용 경로를 stwist로 설정. 풀네임으로 하기에는 경로가 너무 길고
      치기 힘들어지므로 줄임말 사용.
    - API 전용 경로를 stwist-api로 확보할 예정
    - 설정파일 코드오너 설정

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('상하의 트위스트 서버 메인');
+app.use(
+  '/stwist/',
+  express.static(path.join(__dirname, 'react-front-app/build')),
+);
+
+//app.use('/stwist-api/',) API 경로 미리 확보
+
+app.get('/stwist/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'react-front-app/build/index.html'));
 });
 
 app.listen(port, () => {
