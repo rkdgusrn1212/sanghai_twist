0fe29ab3e3bc461456bab13891068b0b895095d7
rkdgusrn1212 <khgkjg12@naver.com>
2022-12-08 21:27:02 +0900

    cors 문제 해결
    - cors 문제를 해결하기 위해 nodejs 서버에 express cors 설정을 해줘야했음. 기본적으로 node  기본적으로 cors를 막기 때문임.
    - 백그라운드 멈추는 스크립트 추가. ~길게 치기 귀찮아서~
    - Home 페이지에서 테스트로 axios로 api 요청하는 코드 작성

--- react-front-app/src/pages/Home.js
+++ react-front-app/src/pages/Home.js
@@ -1,9 +1,64 @@
 import CommonHeader from '../components/common/CommonHeader';
+import Carousel from 'react-bootstrap/Carousel';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [bannerImgs, setBannerImgs] = new useState([]);
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'http://192.168.0.115:8080/stwist-api/category',
+      responseType: 'JSON',
+    }).then((res) => {
+      const tempBannerImgs = [];
+      console.log(res);
+    });
+  }, []);
+
   return (
     <>
       <CommonHeader />
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=First slide&bg=373940"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Second slide&bg=282c34"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Third slide&bg=20232a"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </>
   );
 };
