d01d3cdbdf71816bf24a72e7c9456ae7daecbb7d
rkdgusrn1212 <khgkjg12@naver.com>
2022-12-08 11:52:12 +0900

    Open API 호출 로직 및 서비스 관련 작업
    - Open API 호출 로직을 services/eleven으로 분리
    - services에 getCatSet.js를 생성, 의류, 패션과 관련된 카테고리 목록을
      반환하게 함.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
-//const dress = require('./dress');
+const productSearch = require('../../services/eleven/productSearch');
+const categoryInfo = require('../../services/eleven/categoryInfo');
+const getCatSet = require('../../services/getCatSet');
+
 
 /**
  * API 요청에 대한 라우팅 정의
  */
-//router.use('/dress', dress);
+router.use('/product', async (req, res)=>{
+    res.send(await productSearch(req.query));
+});
+
+
+router.use('/category', async (req, res)=>{
+    res.send(await categoryInfo(req.query));
+});
+
+router.use('/set', async (req, res)=>{
+    res.send(await getCatSet());
+});
 
 module.exports = router;
