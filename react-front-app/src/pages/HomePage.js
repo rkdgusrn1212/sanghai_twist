3ba7203960ca0d15d230bb3ae55ace851c527781
rkdgusrn1212 <khgkjg12@naver.com>
2022-12-09 14:06:25 +0900

    배너에 카테고리 이름과 링크 같이 띄움

--- react-front-app/src/pages/HomePage.js
+++ react-front-app/src/pages/HomePage.js
@@ -3,21 +3,22 @@ import Carousel from 'react-bootstrap/Carousel';
 import Container from 'react-bootstrap/Container';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 
 const IMG_PER_BANNER = 3; //배너 하나당 포함되는 사진 수
 
-const findLeafCat = (category, leafList)=>{
-  if(category.child){
-    for(let sub of category.child){
+const findLeafCat = (category, leafList) => {
+  if (category.child) {
+    for (let sub of category.child) {
       findLeafCat(sub, leafList);
     }
-  }else{
+  } else {
     leafList.push(category);
   }
-}
+};
 
 const Home = () => {
-  const [bannerImgs, setBannerImgs] = new useState([]);
+  const [banners, setBanners] = new useState([]);
 
   useEffect(() => {
     axios({
@@ -28,57 +29,58 @@ const Home = () => {
       .then((res) => {
         let banners = [];
         let catList = [];
-        for(let sub of res.data.categories){
+        for (let sub of res.data.categories) {
           findLeafCat(sub, catList);
         }
         console.log(catList);
         Promise.all(
-          catList.map((cat) =>
-            axios({
-              method: 'get',
-              url: `http://192.168.0.115:8080/stwist-api/category/${cat.code}?size=${IMG_PER_BANNER}&srt=I`,
-              responseType: 'json',
-            }).then((res) => {
-              const category = res.data.category;
-              const banner = {
-                name : category.name,
-                code : category.code,
-                image : res.data.products.items.map((item) => item.image)
-              };
-              if(banner.image.length>=IMG_PER_BANNER){
-                banners.push(banner);
-              }
-            }),//items가 없는 카테고리도 있음, BANNER를 꽉 체울 크기인 것들만 집어넣음.
+          catList.map(
+            (cat) =>
+              axios({
+                method: 'get',
+                url: `http://192.168.0.115:8080/stwist-api/category/${cat.code}?size=${IMG_PER_BANNER}&srt=I`,
+                responseType: 'json',
+              }).then((res) => {
+                const category = res.data.category;
+                const banner = {
+                  name: category.name,
+                  code: category.code,
+                  image: res.data.products.items.map((item) => item.image),
+                };
+                if (banner.image.length >= IMG_PER_BANNER) {
+                  banners.push(banner);
+                }
+              }), //items가 없는 카테고리도 있음, BANNER를 꽉 체울 크기인 것들만 집어넣음.
           ),
         ).then(() => {
           console.log(banners);
-          setBannerImgs(banners);
+          setBanners(banners);
         });
       })
       .catch((e) => console.log(e));
-  }, [setBannerImgs]);
+  }, [setBanners]);
 
   return (
     <>
       <CommonHeader />
       <Container fluid="lg" />
       <Carousel>
-        {bannerImgs.map((imgs, i) => (
+        {banners.map((banner, i) => (
           <Carousel.Item key={i}>
             <div className="d-flex justify-content-center w-100">
-              {imgs.image.map((img, i) => (
+              {banner.image.map((img, i) => (
                 <img
                   src={img}
                   key={i}
-                  alt="First slide"
+                  alt={i + '번 이미지'}
                   width="300"
                   height="300"
                 />
               ))}
             </div>
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3>{banner.name}</h3>
+              <Link to={'list?code='+banner.code}>상품 보러가기</Link>
             </Carousel.Caption>
           </Carousel.Item>
         ))}
