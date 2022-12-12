6733b821fb8d9e85450354808cd680fe57bd3857
rkdgusrn1212 <khgkjg12@naver.com>
2022-12-12 15:27:40 +0900

    대분류 컴포넌트 이미지 주소 로드전, 혹은 주소가 없을 때 대체 이미지 뜨게함

--- react-front-app/src/components/home/MajorCategory.js
+++ react-front-app/src/components/home/MajorCategory.js
@@ -1,42 +1,39 @@
+import { useEffect } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Placeholder from 'react-bootstrap/Placeholder';
+import { run as runHolder } from 'holderjs';
 import { useGetCategoryInfo } from '../../hooks';
-import 'holderjs';
+
+const cardStyle = {
+  width: "300px",
+}
 
 const MajorCategory = ({ category }) => {
-  const { categoryInfo, isSuccess } = 
-  useGetCategoryInfo({
+  const { categoryInfo, isSuccess } = useGetCategoryInfo({
     code: category.code,
     pg: 1,
     srt: 'I',
   });
 
-  return isSuccess ? (
-    <Card>
-      <LazyLoadImage
-        className="card-img-top"
-        src={categoryInfo.products.items[0].image.high}
-      ></LazyLoadImage>
+  useEffect(() => {
+    runHolder({
+      images: '#major-category-img-holder',
+    });
+  }, []);
+
+  return (
+    <Card style={cardStyle}>
+      {isSuccess && categoryInfo.products.items[0] ? (
+        <LazyLoadImage
+          className="card-img-top"
+          src={categoryInfo.products.items[0].image.high}
+        />
+      ) : (
+        <Card.Img id="major-category-img-holder" src="holder.js/100px300" />
+      )}
       <Card.Body>
-        <Card.Title>{categoryInfo.category.name}</Card.Title>
-        <Card.Text>{categoryInfo.category.child} </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-  ) : (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" data-src="holder.js/100px300"></Card.Img>
-      <Card.Body>
-        <Placeholder as={Card.Title} animation="glow">
-          <Placeholder xs={6} />
-        </Placeholder>
-        <Placeholder as={Card.Text} animation="glow">
-          <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
-          <Placeholder xs={6} /> <Placeholder xs={8} />
-        </Placeholder>
-        <Placeholder.Button variant="primary" xs={6} />
+        <Card.Title>{category.name}</Card.Title>
+        <Card.Text></Card.Text>
       </Card.Body>
     </Card>
   );
