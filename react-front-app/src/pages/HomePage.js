54957070163f6a5fe1a10e678649f832d938b910
rkdgusrn1212 <khgkjg12@naver.com>
2022-12-13 17:09:32 +0900

    CategoryTree를 CategoryGrid에서 HomePage로 옮김
    - 그리고 나머지 수정 파일들은 Prettier 한번 돌린것.

--- react-front-app/src/pages/HomePage.js
+++ react-front-app/src/pages/HomePage.js
@@ -2,14 +2,24 @@ import CommonHeader from '../components/common/CommonHeader';
 import Banner from '../components/home/Banner';
 import CategoryGrid from '../components/home/CategoryGrid';
 import CategoryFixed from '../components/home/CategoryFixed';
+import CategoryTree from '../components/home/CategoryTree';
+import { useGetCategoryList } from '../hooks';
 
 const Home = () => {
+  const { categoryList, isSuccess } = useGetCategoryList();
   return (
     <>
       <CommonHeader />
       <Banner />
+      <CategoryTree
+        data={isSuccess && categoryList}
+        onClickItem={({ key, name, ...props }) => {
+          this.navigate(props.url);
+        }}
+        debounceTime={125}
+      />
       <CategoryGrid />
-      <CategoryFixed/>
+      <CategoryFixed />
     </>
   );
 };
