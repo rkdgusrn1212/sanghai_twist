0f106dc626fabef59a4c9bebe291d60e2cc4ec85
rkdgusrn1212 <khgkjg12@naver.com>
2022-12-13 19:37:10 +0900

    CategoryTree를 함수 컴포넌트화 하고 데이터 패치 훅 사용

--- react-front-app/src/pages/HomePage.js
+++ react-front-app/src/pages/HomePage.js
@@ -3,21 +3,13 @@ import Banner from '../components/home/Banner';
 import CategoryGrid from '../components/home/CategoryGrid';
 import CategoryFixed from '../components/home/CategoryFixed';
 import CategoryTree from '../components/home/CategoryTree';
-import { useGetCategoryList } from '../hooks';
 
 const Home = () => {
-  const { categoryList, isSuccess } = useGetCategoryList();
   return (
     <>
       <CommonHeader />
       <Banner />
-      <CategoryTree
-        data={isSuccess && categoryList}
-        onClickItem={({ key, name, ...props }) => {
-          this.navigate(props.url);
-        }}
-        debounceTime={125}
-      />
+      <CategoryTree/>
       <CategoryGrid />
       <CategoryFixed />
     </>
