e890c07e2fe6883223fc40a156e56f17b7bd4f64
rkdgusrn1212 <khgkjg12@naver.com>
2022-12-14 17:48:29 +0900

    CategoryGrid에서 MajorCategory를 클릭할때 해당 Category의 Minorr Category들이 뜨게함

--- react-front-app/src/components/home/CategoryGrid.js
+++ react-front-app/src/components/home/CategoryGrid.js
@@ -1,16 +1,45 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Spinner from 'react-bootstrap/Spinner';
-import { useCallback, useState, useMemo } from 'react';
+import Stack from 'react-bootstrap/Stack';
+import { useCallback, useState, useEffect } from 'react';
 import { useGetCategoryList } from '../../hooks';
 import MajorCategory from './MajorCategory';
+import MinorCategory from './MinorCategory';
+
+/**
+ * 해당 카테고리와 그 하위의 리프 카테고리를 리프 리스트에 추가해준다.
+ * 인자의 카테고리가 리프 카테고리면 리스트에 넣고 아니면 자식 리스트에 대해 재귀 호출
+ * @param {*} category 검사 대상 카테고리
+ * @param {*} leafList 리프 카테고리들이 저장될 리스트
+ */
+const findLeafCat = (category, leafList, parent) => {
+  if (category.child) {
+    parent.push(category.name);
+    for (let sub of category.child) {
+      findLeafCat(sub, leafList, parent);
+    }
+    parent.pop();
+  } else {
+    leafList.push({...category, parent:parent.join('>')});
+  }
+};
 
 const CategoryGrid = () => {
   const [shrink, setShrink] = useState(false);
   const [selected, setSelected] = useState(null);
+  const [leafCategories, setLeafCategories] = useState([]);
 
   const { categoryList, isSuccess } = useGetCategoryList();
+
+  useEffect(() => {
+    if (!isSuccess) return;
+    let catList = [];
+    for (let sub of categoryList.categories) {
+      if (sub.code === selected) {
+        findLeafCat(sub, catList, []);
+      }
+    }
+    setLeafCategories(catList);
+  }, [selected, categoryList, isSuccess, setLeafCategories]);
 
   const handleClick = useCallback((key, nextSelected) => {
     if (nextSelected) {
@@ -23,23 +52,43 @@ const CategoryGrid = () => {
   }, []);
 
   return (
-    <div className="d-flex">
-          {isSuccess ? (
-            categoryList.categories.map((category) => (
-              <MajorCategory
-                shrink={shrink}
-                selected={selected === category.code}
-                category={category}
-                key={category.code}
-                onClick={(nextSelected) =>
-                  handleClick(category.code, nextSelected)
-                }
-              />
-            ))
-          ) : (
-            <Spinner animation="grow" />
-          )}
-    </div>
+    <Stack gap={2}>
+      <div className="d-flex flex-wrap">
+        {isSuccess ? (
+          categoryList.categories.map((category) => (
+            <MajorCategory
+              key={category.code}
+              shrink={shrink}
+              selected={selected === category.code}
+              category={category}
+              onClick={(nextSelected) =>
+                handleClick(category.code, nextSelected)
+              }
+            />
+          ))
+        ) : (
+          <Spinner animation="grow" />
+        )}
+      </div>
+      <div className="d-flex flex-wrap">
+        {isSuccess ? (
+          leafCategories.map((category) => (
+            <MinorCategory
+              key={category.code}
+              shrink={shrink}
+              selected={selected === category.code}
+              category={category}
+              onClick={(nextSelected) =>
+                handleClick(category.code, nextSelected)
+              }
+              enabled={shrink}
+            />
+          ))
+        ) : (
+          <Spinner animation="grow" />
+        )}
+      </div>
+    </Stack>
   );
 };
 export default CategoryGrid;
---
+++ react-front-app/src/components/home/MinorCategory.js
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import Card from 'react-bootstrap/Card';
+import { run as runHolder } from 'holderjs';
+import { useGetCategoryInfo } from '../../hooks';
+import './MinorCategory.scss';
+
+const MinorCategory = ({ category, enabled }) => {
+  const { categoryInfo, isSuccess } = useGetCategoryInfo({
+    code: category.code,
+    pg: 1,
+    srt: 'I',
+  });
+
+  useEffect(() => {
+    runHolder({
+      images: '#minor-category-img-holder',
+    });
+  }, []);
+
+  return (
+    <Card
+      className={[
+        'minor-category-card-disabled',
+        enabled && 'minor-category-card-enabled',
+      ].join(' ')}
+    >
+      {isSuccess && categoryInfo.products.items[0] ? (
+        <LazyLoadImage
+          className="card-img-top"
+          src={categoryInfo.products.items[0].image.high}
+        />
+      ) : (
+        <Card.Img id="minor-category-img-holder" src="holder.js/100px200" />
+      )}
+      <Card.Body>
+        <Card.Title style={{ fontSize: 'inherit' }}>{category.name}</Card.Title>
+        <Card.Text style={{ fontSize: 'inherit', color:'darkgray' }}><small>{category.parent}</small></Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+export default MinorCategory;
