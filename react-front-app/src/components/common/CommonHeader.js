8c772386c0247ab731318a40579491150f9ae00e
rkdgusrn1212 <khgkjg12@naver.com>
2022-12-15 15:18:57 +0900

    CommonHeader 레이아웃, 기능 수정
    - 검색컴포넌트 추가
    - 스타일 수정
    - 기타 작업

--- react-front-app/src/components/common/CommonHeader.js
+++ react-front-app/src/components/common/CommonHeader.js
@@ -1,9 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { useMemo } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useMemo, useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const CommonHeader = ({ active }) => {
+  const [keyword, setKeyword] = useState('');
+  const navigate = useNavigate();
   const pages = useMemo(() => {
     const result = {
       '': {
@@ -19,26 +24,70 @@ const CommonHeader = ({ active }) => {
     return result;
   }, [active]);
 
+  const handleChange = useCallback((e) => {
+    setKeyword(e.target.value);
+  }, []);
+
+  const handleSubmit = useCallback(
+    (e) => {
+      navigate(process.env.PUBLIC_URL + '?kwd=' + keyword);
+      e.preventDefault();
+    },
+    [keyword, navigate],
+  );
+
   return (
-    <Navbar
-      bg="primary"
-      variant="dark"
-      expand="lg"
-      style={{ fontFamily: 'cafe-surround' }}
-    >
+    <Navbar bg="primary" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home" style={{ fontFamily: 'cafe-classic' }}>
+        <Navbar.Brand
+          href={process.env.PUBLIC_URL}
+          style={{ fontFamily: 'cafe-classic' }}
+        >
           S.TWIST
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="justify-content-between"
+        >
           <Nav className="me-auto">
             {Object.entries(pages).map(([key, value]) => (
-              <Nav.Link href={'/stwist' + key} key={key} active={value.active}>
+              <Nav.Link
+                href={process.env.PUBLIC_URL + key}
+                key={key}
+                active={value.active}
+                style={{ fontFamily: 'cafe-surround' }}
+              >
                 {value.name}
               </Nav.Link>
             ))}
           </Nav>
+          <Form
+            className="d-flex"
+            style={{
+              fontFamily: 'cafe-simplehae',
+            }}
+            onSubmit={handleSubmit}
+          >
+            <Form.Control
+              type="search"
+              placeholder="카테고리 검색"
+              className="me-2"
+              aria-label="Search"
+              value={keyword}
+              onChange={handleChange}
+            />
+            <Button
+              type="submit"
+              variant="buttonPrimary"
+              style={{
+                color: 'white',
+                whiteSpace: 'nowrap',
+              }}
+            >
+              검색
+            </Button>
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
