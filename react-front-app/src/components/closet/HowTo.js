4728f8ee2de3395f73d0ad3dba9620806be9d92c
integer <integer402@gmail.com>
2022-12-15 20:22:51 +0900

    사용법 modal 완성 npm youtube 라이브러리 추가

--- react-front-app/src/components/closet/HowTo.js
+++ react-front-app/src/components/closet/HowTo.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
-import step1 from '../closet/step1.jpg';
+import step1 from '../closet/step1.png';
 import step2 from '../closet/step2.png';
+import step3 from '../closet/step3.png';
 
+import YouTube from 'react-youtube';
 function HowTo() {
   const [show, setShow] = useState(false);
 
@@ -30,15 +32,16 @@ function HowTo() {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>1. 비교하고싶은 옷을 선택한다</p>
+          <YouTube videoId="mH0lRACIvtY" className="youTube" />{' '}
+          <p>Step1. 비교하고싶은 옷을 선택한다</p>
           <img src={step1} className="step1" />
-          <p>사진 1,2번중 하나를 선택</p>
-
           <p>
-            2. 옷 사진중 원하는 부분을 마우스로 드래그해 크롭하고 위치를
+            Step2. 옷 사진중 원하는 부분을 마우스로 드래그해 크롭하고 위치를
             마우스로 조정한다
           </p>
-          <img src={step2} className="step1" />
+          <img src={step2} className="step2" />
+          <p>Step3. 마우스로 섬세하게 조정하기 :)</p>
+          <img src={step3} className="step3" />
         </Modal.Body>
       </Modal>
     </>
