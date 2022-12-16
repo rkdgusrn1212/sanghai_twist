6497a14de2a96476631fe46f62227ac388cb1279
integer <integer402@gmail.com>
2022-12-16 10:09:21 +0900

    SideDetail 구매페이지 이동, 삭제 기능

--- react-front-app/src/components/closet/SideDetail.js
+++ react-front-app/src/components/closet/SideDetail.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './Cropper.css';
-import { Nav, Row } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { useGetProductInfo } from '../../hooks';
 import { useParams } from 'react-router-dom';
-
+import Trash from '../closet/trash.png';
+import Cart from '../closet/cart.png';
+import Del from '../closet/DeleteModal';
 function SideDetail(props) {
   const { productInfo, isUninitialized, isLoading, isError, isSuccess } =
     useGetProductInfo(props.elem);
-
+  const moveShoppingmall = () => {
+    window.open(`https://www.11st.co.kr/products/ + ${props.elem}`, '_blank');
+  };
+  const deleteStorage = () => {
+    console.log(props.elem);
+    localStorage.removeItem(props.elem);
+  };
   if (isSuccess === false) {
     return (
       <>
@@ -19,6 +27,20 @@ function SideDetail(props) {
   } else {
     return (
       <div className="cards">
+        <div className="btnArea">
+          <img
+            className="btnGo"
+            src={Cart}
+            style={{ width: '40px' }}
+            onClick={moveShoppingmall}
+          />
+          <img
+            className="btnDel"
+            src={Trash}
+            style={{ width: '40px' }}
+            onClick={deleteStorage}
+          />
+        </div>
         <img src={productInfo.image} style={{ width: '250px' }} />
         <h6>{productInfo.name}</h6>
       </div>
